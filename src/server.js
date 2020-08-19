d282db1055a2a27e8b96a22953e784d2aa85cb15
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-08-19 20:35:32 -0300

    mandando mail

--- src/server.js
+++ src/server.js
@@ -1,8 +1,39 @@
 /*const path  = require('path')*/
 const express = require('express')
 const app = express()
-/*const  publicDirectory = path.join(__dirname, '../public/')*/
 
-app.listen(3000, function(err){
-    if(err) throw err;
+/*const  publicDirectory = path.join(__dirname, '../public/')*/
+var mailDestino
+var nombre
+app.push('/',function(req,res){
+    formulario = res.body
+    mailDestino = formulario.mail;
+    nombre = formulario.nombre;
+});
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'diplouade2020@gmail.com',
+    pass: 'U4D32020!!'
+  }
+});
+
+var descripcion = 'Estimad@' + nombre + '/n' + 'Contestamos a su duda'
+
+var mailOptions = {
+  from: 'diplouade2020@gmail.com',
+  to: mailDestino,
+  subject: 'Info DiploUade',
+  text: descripcion
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
