c16c5164ef1d2a928053462b2d1e66d65c07dfe6
vperalta19 <56236510+vperalta19@users.noreply.github.com>
2020-08-19 20:15:55 -0300

    todo listo

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const path  = require('path')
+/*const path  = require('path')*/
 const express = require('express')
 const app = express()
-const  publicDirectory = path.join(__dirname, '../public/')
+/*const  publicDirectory = path.join(__dirname, '../public/')*/
 
 app.listen(3000, function(err){
     if(err) throw err;
